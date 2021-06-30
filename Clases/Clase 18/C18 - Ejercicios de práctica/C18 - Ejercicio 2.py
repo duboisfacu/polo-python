3ba5aa11f8fc82a7c0b1cdf6e937ee5dcca22345
duboisfacu <facundubois@hotmail.com>
2021-06-30 20:15:00 -0300

    ♥

--- Clases/Clase 18/C18 - Ejercicios de práctica/C18 - Ejercicio 2.py
+++ Clases/Clase 18/C18 - Ejercicios de práctica/C18 - Ejercicio 2.py
@@ -9,14 +9,20 @@
 
 
 lista=[]
-
+opcion= None
 
 def Suma():
     suma= 0
     cont= 1
+    print("-------------------------------------------")
     print("Has seleccionado SUMA!")
+    print("-------------------------------------------")
     cantop = (input(f"Cuántos números queres sumar?: "))
-    if cantop.isnumeric() == True:
+    if cantop == "1":
+        print("-------------------------------------------")
+        print("Se suma de a 2 números crack.")
+        print("-------------------------------------------")
+    elif cantop.isnumeric() == True:
         for x in range(0,int(cantop)):
             num = (input(f"Ingresa el número {cont} a sumar: "))
             if num.isnumeric() == True:
@@ -24,23 +30,154 @@ def Suma():
                 suma += int(num)
                 lista.append(num)
             else:
+                print("-------------------------------------------")
                 print("Número no válido.")
-            print(f"La suma total de los números es de: {suma}")
+                print("-------------------------------------------")
+                break
+        print("-------------------------------------------")
+        print(f"La suma total de los números es de: {suma}")
+        print("-------------------------------------------")
     else:
-        print("Número no válido.") 
-        Suma()
+        print("Número no válido.")
+        print("-------------------------------------------")
+        Inicio()
+
+def Resta():
+    lista.clear()
+    resta= 0
+    cont= 1
+    print("-------------------------------------------")
+    print("Has seleccionado RESTA!")
+    print("-------------------------------------------")
+    cantop = (input(f"Cuántos números queres restar?: "))
+    if cantop == "1":
+        print("-------------------------------------------")
+        print("Se resta de a 2 números crack.")
+        print("-------------------------------------------")
+    elif cantop.isnumeric() == True:
+        for x in range(0,int(cantop)):
+            num = (input(f"Ingresa el número {cont} a restar: "))
+            if num.isnumeric() == True:
+                cont +=1
+                if len(lista) == 0:
+                    resta = int(num)
+                    lista.append(num)
+                else:
+                    resta = resta - int(num)
+                    lista.append(num)    
+            else:
+                print("-------------------------------------------")
+                print("Número no válido.")
+                print("-------------------------------------------")
+                break
+        print("-------------------------------------------")
+        print(f"La resta total de los números es de: {resta}")
+        print("-------------------------------------------")
+    else:
+        print("Número no válido.")
+        print("-------------------------------------------")
+        Inicio()
+
+def Multiplicacion():
+    lista.clear()
+    multiplicacion= 0
+    cont= 1
+    print("-------------------------------------------")
+    print("Has seleccionado MULTIPLICACIÓN!")
+    print("-------------------------------------------")
+    cantop = (input(f"Cuántos números queres multiplicar?: "))
+    if cantop == "1":
+        print("-------------------------------------------")
+        print("Se multiplica de a 2 números crack.")
+        print("-------------------------------------------")
+    elif cantop.isnumeric() == True:
+        for x in range(0,int(cantop)):
+            num = (input(f"Ingresa el número {cont} a multiplicar: "))
+            if num.isnumeric() == True:
+                cont +=1
+                if len(lista) == 0:
+                    multiplicacion = int(num)
+                    lista.append(num)
+                else:
+                    multiplicacion = multiplicacion * int(num)
+                    lista.append(num)    
+            else:
+                print("-------------------------------------------")
+                print("Número no válido.")
+                print("-------------------------------------------")
+                break
+        print("-------------------------------------------")
+        print(f"La multiplicación total de los números es de: {multiplicacion}")
+        print("-------------------------------------------")
+    else:
+        print("Número no válido.")
+        print("-------------------------------------------")
+        Inicio()
+
+def Division():
+    lista.clear()
+    division= 0
+    cont= 1
+    print("-------------------------------------------")
+    print("Has seleccionado DIVISIÓN!")
+    print("-------------------------------------------")
+    cantop = (input(f"Cuántos números queres dividir?: "))
+    if cantop == "1":
+        print("-------------------------------------------")
+        print("Se divide de a 2 números crack.")
+        print("-------------------------------------------")
+    elif cantop.isnumeric() == True:
+        for x in range(0,int(cantop)):
+            num = (input(f"Ingresa el número {cont} a dividir: "))
+            if num.isnumeric() == True:
+                cont +=1
+                if len(lista) == 0:
+                    division = int(num)
+                    lista.append(num)
+                else:
+                    division = division / int(num)
+                    lista.append(num)    
+            else:
+                print("-------------------------------------------")
+                print("Número no válido.")
+                print("-------------------------------------------")
+                break
+        print("-------------------------------------------")
+        print(f"La división total de los números es de: {division}")
+        print("-------------------------------------------")
+    else:
+        print("Número no válido.")
+        print("-------------------------------------------")
+        Inicio()
+
+
+
+
+
+
 
 def Inicio():
+    
     print("Calculadora super pro:")
+    print("-------------------------------------------")
+    opcion = input("Opción a realizar: \n1 - Sumar \n2 - Restar\n3 - Multiplicar\n4 - Dividir\n")
 
-    print("Opción a realizar: \n1 - Sumar \n2 - Restar\n3 - Multiplicar\n4 - Dividir")
-
-    return input("Qué opción queres usar?").upper
-
-Inicio()
-
-    if return != "1" or opcion == "SUMAR":
+    if opcion == "1" or opcion == "SUMAR":
         Suma()
+        Inicio()
+    elif opcion == "2" or opcion == "RESTAR":
+        Resta()
+        Inicio()
+    elif opcion == "3" or opcion == "MULTIPLICAR":
+        Multiplicacion()
+        Inicio()
+    elif opcion == "4" or opcion == "DIVIDIR":
+        Division()
+        Inicio()
     else:
         print("Opción no válida.") 
         Inicio()
+
+Inicio()
+
+
